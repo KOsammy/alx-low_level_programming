ccb902f55157edc5e5e3f700a4dcbed4bafa2630
KOsammy <samoppong770@gmail.com>
2024-06-21 18:05:51 -0700

    6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,20 +3,20 @@
 /**
  * main - Entry point
  *
- * Description: Prints all single-digit numbers of base 10,
- *              starting from 0, followed by a new line.
+ * Description: Prints all single digit numbers of base 10 starting from 0,
+ *              followed by a new line, using putchar function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int digit;
+	int digit;
 
-    for (digit = 0; digit < 10; digit++)
-    {
-        putchar(digit + '0');
-    }
-    putchar('\n');
-    return (0);
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit + '0');
+	}
+	putchar('\n');
+	return (0);
 }
 
