3868912042712b8a49531f6ab98a29ea4e6b53fe
KOsammy <samoppong770@gmail.com>
2024-06-21 18:16:32 -0700

    7-print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,21 +4,21 @@
  * main - Entry point
  *
  * Description: Prints the lowercase alphabet in reverse,
- *              followed by a new line.
+ *              followed by a new line. Uses putchar twice only.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char letter = 'z'; /* Start with the last lowercase letter */
+	char ch = 'z'; /* Start with the last lowercase letter */
 
-    while (letter >= 'a')
-    {
-        putchar(letter); /* Print the current letter */
-        letter--;       /* Move to the previous letter */
-    }
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--; /* Decrement to print the next character in reverse */
+	}
 
-    putchar('\n'); /* Print the newline character */
-    return (0);
+	putchar('\n'); /* Print the newline character */
+	return (0);
 }
 
