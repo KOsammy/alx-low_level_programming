cc3893b5059371b1c5d1be0c14784b05755a7b0b
KOsammy <samoppong770@gmail.com>
2024-06-25 17:33:46 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+int _putchar(char c);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char *str = "_putchar";
+    while (*str)
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
+    return 0;
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This new directory is still on the alx c programming learning journey and,
+I am starting to learn about functions and nested loops in c.
