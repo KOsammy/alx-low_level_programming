f115e49fda825360b2cc84b49e9e1b1c3b1b6bd7
KOsammy <samoppong770@gmail.com>
2024-06-25 20:53:30 -0700

    100-print_times table file added

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: integer to specify the size of the times table
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+    int i, j, product;
+
+    if (n < 0 || n > 15)
+        return;
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            product = i * j;
+            if (j == 0)
+                printf("%d", product);
+            else
+                printf(", %3d", product);
+        }
+        printf("\n");
+    }
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,6 +15,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int x);
+void print_times_table(int n);
 
 #endif /* MAIN_H */
 
