a6811ec25ba5f959853bf1e98990cdab51c5181f
KOsammy <samoppong770@gmail.com>
2024-06-25 20:59:12 -0700

    Betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,35 +10,35 @@
  */
 void print_times_table(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int result = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int result = i * j;
 
-            if (j == 0)
-                _putchar('0' + result);
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (result >= 100)
-                    _putchar('0' + result / 100);
-                else
-                    _putchar(' ');
-                if (result >= 10)
-                    _putchar('0' + (result / 10) % 10);
-                else
-                    _putchar(' ');
-                _putchar('0' + result % 10);
-            }
-        }
-        _putchar('\n');
-    }
+			if (j == 0)
+				_putchar('0' + result);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result >= 100)
+					_putchar('0' + result / 100);
+				else
+					_putchar(' ');
+				if (result >= 10)
+					_putchar('0' + (result / 10) % 10);
+				else
+					_putchar(' ');
+				_putchar('0' + result % 10);
+			}
+		}
+		_putchar('\n');
+	}
 }
 
