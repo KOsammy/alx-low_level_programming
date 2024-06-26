4662c6586efa3e3dfecc16a918ef81fb469ce631
KOsammy <samoppong770@gmail.com>
2024-06-25 21:24:42 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *   Prints the first 98 Fibonacci numbers separated by comma and space.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int count = 0;
+    unsigned long fib1 = 1, fib2 = 2, next;
+
+    printf("%lu, %lu", fib1, fib2);
+    count = 2;
+
+    while (count < 98)
+    {
+        next = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = next;
+
+        printf(", %lu", next);
+        count++;
+    }
+
+    printf("\n");
+
+    return (0);
+}
+
