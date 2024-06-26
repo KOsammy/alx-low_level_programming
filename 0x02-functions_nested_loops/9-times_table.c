9f7915ffa848a192d86dae0783dc209d2058ab25
KOsammy <samoppong770@gmail.com>
2024-06-25 20:11:56 -0700

    Corrected

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,36 +5,36 @@
  */
 void times_table(void)
 {
-	int row, column, product;
+    int row, column, product;
 
-	for (row = 0; row <= 9; row++)
-	{
-		for (column = 0; column <= 9; column++)
-		{
-			product = row * column;
+    for (row = 0; row <= 9; row++)
+    {
+        for (column = 0; column <= 9; column++)
+        {
+            product = row * column;
 
-			if (column == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
+            if (column == 0)
+            {
+                _putchar('0' + product);
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
 
-				if (product >= 10)
-					_putchar('0' + (product / 10));
-				else
-					_putchar(' ');
+                if (product >= 10)
+                    _putchar('0' + (product / 10));
+                else
+                    _putchar(' ');
 
-				_putchar('0' + (product % 10));
-			}
+                _putchar('0' + (product % 10));
+            }
 
-			if (column < 9)
-				_putchar(' ');
-			else
-				_putchar('\n');
-		}
-	}
+            if (column < 9)
+                _putchar(',');
+            else
+                _putchar('\n');
+        }
+    }
 }
 
