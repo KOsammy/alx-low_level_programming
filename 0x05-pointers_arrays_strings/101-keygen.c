ace1f71c743b3a56114f3147bd6862210f0873d4
KOsammy <samoppong770@gmail.com>
2024-07-01 14:47:58 -0700

    Added 101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Generates a random valid password for 101-crackme
+ * main - Generates a random valid password of 15 characters for 101-crackme
  *
  * Return: Always 0
  */
@@ -12,23 +12,23 @@ int main(void)
     int i;
     int random_char;
     int sum = 0;
-    char password[11]; /* 10 characters + '\0'*/
+    char password[16]; /* 15 characters + '\0' */
 
     srand(time(NULL)); /* Seed the random number generator */
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 15; i++)
     {
         random_char = rand() % (126 - 32 + 1) + 32; /* Generate random ASCII character in the range [32, 126] */
         password[i] = (char)random_char; /* Convert integer to character and store in password array */
         sum += random_char; /* Calculate sum of ASCII values for validation in 101-crackme */
     }
-    password[10] = '\0'; /* Null-terminate the password string */
+    password[15] = '\0'; /* Null-terminate the password string */
 
-    /* Print the generated password*/
+    /* Print the generated password */
     printf("%s\n", password);
 
-    /* Print the sum of ASCII values for validation in 101-crackme*/
-    printf("%d\n", 2772 - sum);
+    /* Print the sum of ASCII values for validation in 101-crackme */
+    printf("%d\n", sum);
 
     return (0);
 }
