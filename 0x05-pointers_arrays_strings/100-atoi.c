8db99345e37f396d6727b7af068c53c2273de512
KOsammy <samoppong770@gmail.com>
2024-07-01 15:02:49 -0700

    Added

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -12,6 +13,7 @@ int _atoi(char *s)
     int sign = 1;
     int result = 0;
     int digit;
+    int prev_result;
 
     while (s[i] != '\0')
     {
@@ -20,7 +22,20 @@ int _atoi(char *s)
         else if (s[i] >= '0' && s[i] <= '9')
         {
             digit = s[i] - '0';
+
+            /* Check for overflow before performing addition */
+            prev_result = result;
             result = result * 10 + digit;
+            
+            /* Overflow check */
+            if ((result - digit) / 10 != prev_result)
+            {
+                /* Overflow detected, return appropriate value */
+                if (sign == 1)
+                    return INT_MAX;
+                else
+                    return INT_MIN;
+            }
         }
         else if (s[i] == '+')
             ; /* Ignore explicit '+' sign */
