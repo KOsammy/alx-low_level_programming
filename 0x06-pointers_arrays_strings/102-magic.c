b6103794b2c35a01e289273830885c8b22b116a2
KOsammy <samoppong770@gmail.com>
2024-07-04 15:43:01 -0700

    Added 102-magic file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: Initializes an integer array 'a', assigns a value to 'a[2]',
- *              creates a pointer 'p' pointing to an integer 'n', and then
- *              adds a statement to indirectly modify 'a[2]'
- *              Finally, prints the modified value of 'a[2]'.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
-	*(p + 5) = 98;  /* This line of code is added */
-
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
 
