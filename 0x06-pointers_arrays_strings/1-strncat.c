e060a5f01cc92a4f612a9da7cc18c1369962325a
KOsammy <samoppong770@gmail.com>
2024-07-04 13:46:20 -0700

    Added the 1-strncat file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates two strings, using at most n bytes from src
+ * _strncat - Concatenates two strings,
+ * using at most n bytes from src
  * @dest: The destination string
  * @src: The source string
  * @n: The maximum number of bytes to be used from src
@@ -10,26 +11,26 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    char *ptr = dest;
+	char *ptr = dest;
 
-    /* Find the end of the destination string */
-    while (*ptr != '\0')
-    {
-        ptr++;
-    }
+	/* Find the end of the destination string */
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
 
-    /* Append the source string to the destination string */
-    while (*src != '\0' && n > 0)
-    {
-        *ptr = *src;
-        ptr++;
-        src++;
-        n--;
-    }
+	/* Append the source string to the destination string */
+	while (*src != '\0' && n > 0)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
 
-    /* Add the terminating null byte */
-    *ptr = '\0';
+	/* Add the terminating null byte */
+	*ptr = '\0';
 
-    return (dest);
+	return (dest);
 }
 
