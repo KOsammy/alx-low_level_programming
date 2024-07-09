d71f2ad124777fbc3a7d80d41d42aab14d0cbdbf
KOsammy <samoppong770@gmail.com>
2024-07-09 12:56:00 -0700

    Added the 100-set_srting files

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to be updated
+ * @to: pointer to be assigned
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
