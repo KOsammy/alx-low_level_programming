cd61e4ab78c57097adcd27b6939cfe8baf596da4
KOsammy <samoppong770@gmail.com>
2024-07-09 08:19:55 -0700

    Added the 2-strchr file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, const char *src, unsigned int n);
-char *_strchr(char *s, char c)
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
 
