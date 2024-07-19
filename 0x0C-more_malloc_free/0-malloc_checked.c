3c9c81dd4541ce1d0c8d475de080e7ed97eb0650
KOsammy <samoppong770@gmail.com>
2024-07-18 18:24:09 -0700

    Added the betty documentation issues

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,9 +3,9 @@
 
 /**
  *malloc_checked - this allocates memory using malloc
- *@b- This is the number of bytes to allocate
+ *@b: This is the number of bytes to allocate
  *
- * Return - This is a pointer to the allocated memory
+ * Return:  This is a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
