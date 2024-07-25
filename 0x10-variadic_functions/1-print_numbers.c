90a668ae8ecad3552fe84c37892177d2684a5f87
KOsammy <samoppong770@gmail.com>
2024-07-25 13:36:08 -0700

    Added the corrections from betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
 
 /**
  * print_numbers -prints numbers, followed by a new line
- * @seperator: the string to be printed between numbers
+ * @separator: the string to be printed between numbers
  * @n: The number of integers passed to the function
  *
  * Return: Nothing
