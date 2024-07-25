8f35aeebe5f220240fbaf0032d1daa5009add226
KOsammy <samoppong770@gmail.com>
2024-07-24 19:11:15 -0700

    Added the betty corrections

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,8 +7,8 @@
  * @cmp: pointer to the function to be used to compare values
  *
  * Return: Index where the first element for which the cmp function
- * 	does not return 0. if no element matches, return -1.
- * 	if size ,=0, return -1.
+ *	does not return 0. if no element matches, return -1.
+ *	if size ,=0, return -1.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
