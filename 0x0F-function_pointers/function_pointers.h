99164dcccd69d8e0488ae76f2ebfb108d459f923
KOsammy <samoppong770@gmail.com>
2024-07-24 18:07:35 -0700

    Corrected the file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 
 
 #endif /* FUNCTION_POINTERS_H */
+
